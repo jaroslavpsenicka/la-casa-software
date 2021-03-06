2df43ab253de1dbb681a1df1d263977c8a7166a1
Jaroslav Psenicka <jaroslav.psenicka@gmail.com>
2021-03-06 17:45:55 +0100

    hierarchy opt

---
+++ client/components/NoNews.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoNews = () => (
+  <div className="mt-5 text-center text-secondary">There are no news, is it good or bad these days?</div>
+)
+
+export default NoNews;
